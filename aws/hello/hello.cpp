8bcd259a36cce0c0f448f78fffb138977ecc637e
y² <yy@yuyuan.org>
2018-10-09 07:12:16 +0000

    beginning of AWS SDK for C++ Developer Guide
    https://docs.aws.amazon.com/sdk-for-cpp/index.html

---
+++ aws/hello/hello.cpp
@@ -0,0 +1,13 @@
+#include <aws/core/Aws.h>
+
+int main(int argc, char** argv)
+{
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+    {
+        // make your SDK calls here.
+        std::cout << "hello aws" << std::endl;
+    }
+    Aws::ShutdownAPI(options);
+    return 0;
+}
