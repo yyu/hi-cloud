6f2133d3945d65164394b91a380978c2a80da77e
y² <yy@yuyuan.org>
2018-10-09 08:34:39 +0000

    polly describes voices

--- aws/hello-polly/hellopolly.cpp
+++ aws/hello-polly/hellopolly.cpp
@@ -1,12 +1,30 @@
 #include <aws/core/Aws.h>
+#include <aws/core/utils/Outcome.h>
+#include <aws/polly/PollyClient.h>
+#include <aws/polly/model/DescribeVoicesRequest.h>
+#include <aws/polly/model/DescribeVoicesResult.h>
 
 int main(int argc, char** argv)
 {
     Aws::SDKOptions options;
     Aws::InitAPI(options);
     {
-        // make your SDK calls here.
-        std::cout << "hello polly" << std::endl;
+        Aws::Polly::PollyClient pollyClient;
+        Aws::Polly::Model::DescribeVoicesRequest describeVoicesRequest;
+
+        const auto & name = pollyClient.GetServiceClientName();
+        std::cout << "I am " << name << std::endl;
+
+        const Aws::Polly::Model::DescribeVoicesOutcome outcome = pollyClient.DescribeVoices(describeVoicesRequest);
+        if (outcome.IsSuccess()) {
+            auto const & result = outcome.GetResult();
+
+            for (const auto & voice : result.GetVoices()) {
+                std::cout << Aws::Polly::Model::VoiceIdMapper::GetNameForVoiceId(voice.GetId()) << std::endl;
+            }
+        } else {
+            std::cout << "no voice returned " << std::endl;
+        }
     }
     Aws::ShutdownAPI(options);
     return 0;
